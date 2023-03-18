f0b19c90307f2ce098ac58f87f97f4aa64e4ab2f
SemogEnaz <SemogEnaz@kittymail.com>
2023-03-19 03:10:34 +1100

    adding brightness contorle shell scripts

---
+++ brightUp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curr_brightness=$(cat /sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-eDP-1/intel_backlight/brightness)
+
+new_brightness=$(($curr_brightness + 100))
+
+echo $new_brightness > /sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-eDP-1/intel_backlight/brightness
